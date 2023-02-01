db0c392aa4dd70135f49449643ff344bb177cff8
computerbomin <computer.bomin@gmail.com>
2023-02-01 16:40:14 +0900

    [ADD] Slider : 슬릭 슬라이더 분리
    슬릭 슬라이더 컴포넌트 분리

--- front-end/src/components/Body.jsx
+++ front-end/src/components/Body.jsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useState } from "react";
 import styled from "styled-components";
-import ImgComponent from "./ImageComponent";
 import Map from "./Map";
 import RegionListModal from "./RegionListModal";
+import Slider from "./Slider";
 
 import { useDispatch, useSelector } from "react-redux";
 import { increase, decrease } from "../modules/mapSize";
@@ -108,47 +108,3 @@ const MapSize = styled(BtnStyle)`
 `;
 
 export default Body;
-
-// ========== Body Slider Wrap 지도 위 슬라이드
-
-const Slider = () => {
-  // const settings = {
-  //     pauseOnHover: true,
-  //     dots: true,
-  //     speed: 500,
-  //     slidesToShow: 1,
-  //     slidesToScroll: 1,
-  //   };
-
-  return (
-    <SliderWrap>
-      {/* <Slider {...settings}> */}
-      <SlideContent />
-      {/* </Slider> */}
-    </SliderWrap>
-  );
-};
-
-const SliderWrap = styled.div`
-  height: 200px;
-  width: 100%;
-  position: absolute;
-  bottom: 0;
-  z-index: 10;
-  background-color: #8f8fec;
-`;
-
-// =========== Body Slider Content
-
-const SlideContent = (props) => {
-  return <SliderContentWrap></SliderContentWrap>;
-};
-
-const SliderContentWrap = styled.div`
-  width: 70%;
-  height: 100%;
-  background: #68b968;
-  border: 1px solid #c09567;
-  border-radius: 10px;
-  margin: auto; // 나중에 지워도 됨
-`;
--- front-end/src/components/ListPage.jsx
+++ front-end/src/components/ListPage.jsx
@@ -1,123 +1,116 @@
-import React from 'react'
-import styled from 'styled-components'
-import ImgComponent from './ImageComponent'
+import React from "react";
+import styled from "styled-components";
+import ImgComponent from "./ImageComponent";
 
-const ListPage = ({detailModalOpen}) => {
+const ListPage = ({ detailModalOpen }) => {
   return (
     <ListPageWrap>
-        <FlexWrap>
-            <ListContent onClick={detailModalOpen} />
-            <ListContent />
-            <ListContent />
-            <ListContent />
-            <ListContent />
-            <ListContent />
-        </FlexWrap>
+      <FlexWrap>
+        <ListContent onClick={detailModalOpen} />
+        <ListContent />
+      </FlexWrap>
     </ListPageWrap>
-  )
-}
+  );
+};
 
 const ListPageWrap = styled.div`
-    width: 100%;
-    box-sizing: border-box;
-    border-radius: 10px; 
-    margin-top: 100px;
-    flex: 1;
-`
+  width: 100%;
+  box-sizing: border-box;
+  border-radius: 10px;
+  margin-top: 100px;
+  flex: 1;
+`;
 
 const FlexWrap = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: flex-start;
-`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
 
-export default ListPage
+export default ListPage;
 
 const ListContent = (props) => {
-    return (
-        <ListContentWrap onClick={props.onClick}>
-            <ImgWrap>
-                <ImgComponent src={'ex01.png'} width={'100%'} />
-            </ImgWrap>
-            <AboutWrap>
-                <Top>
-                    <Title>명동교자</Title>
-                    <Address>서울 종로구 종로4길 33 (청진동)</Address>
-                </Top>
-                <Middle>
-                    <Scope>2.8</Scope>
-                    <ScopeIConWrap></ScopeIConWrap>
-                </Middle>
-                <Bottom>리뷰 {props.reviewNum}</Bottom>
-            </AboutWrap>
-        </ListContentWrap>
-    )
-}
+  return (
+    <ListContentWrap onClick={props.onClick}>
+      <ImgWrap>
+        <ImgComponent src={"ex01.png"} width={"100%"} />
+      </ImgWrap>
+      <AboutWrap>
+        <Top>
+          <Title>명동교자</Title>
+          <Address>서울 종로구 종로4길 33 (청진동)</Address>
+        </Top>
+        <Middle>
+          <Scope>2.8</Scope>
+          <ScopeIConWrap></ScopeIConWrap>
+        </Middle>
+        <Bottom>리뷰 {props.reviewNum}</Bottom>
+      </AboutWrap>
+    </ListContentWrap>
+  );
+};
 
 const ListContentWrap = styled.div`
-    background: #FFFFFF;
-    border: 1px solid #C09567;
-    border-radius: 10px;
-    width: 49%;
-    margin-bottom: 2.5%;
-    padding: 25px;
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-`
+  background: #ffffff;
+  border: 1px solid #c09567;
+  border-radius: 10px;
+  width: 49%;
+  margin-bottom: 2.5%;
+  padding: 25px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+`;
 // ====== 왼쪽 이미지
 const ImgWrap = styled.div`
-    height: 150px;
-    width: 40%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    border-radius: 10px;
-    margin-right: 25px;
-`
+  height: 150px;
+  width: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  border-radius: 10px;
+  margin-right: 25px;
+`;
 // ====== 오른쪽 설명
 const AboutWrap = styled.div`
-    flex: 1;
-`
+  flex: 1;
+`;
 // 매장명 & 주소
 const Top = styled.div`
-    margin-bottom: 20px;
-    display: flex;
-    align-items: baseline;
-`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: baseline;
+`;
 const Title = styled.span`
-    font-weight: 700;
-    font-size: 30px;
-    color: #000000;
-    margin-right: 10px;
-`
+  font-weight: 700;
+  font-size: 30px;
+  color: #000000;
+  margin-right: 10px;
+`;
 const Address = styled.span`
-    font-weight: 400;
-    font-size: 15px;
-    color: #999999;
-    width: 50%;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-`
+  font-weight: 400;
+  font-size: 15px;
+  color: #999999;
+  width: 50%;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`;
 // 별점 & 아이콘
 const Middle = styled.div`
-    margin-bottom: 20px;
-`
+  margin-bottom: 20px;
+`;
 const Scope = styled.span`
-    font-weight: 700;
-    font-size: 30px;
-    color: #000000;
-`
-const ScopeIConWrap = styled.div`
-    
-`
+  font-weight: 700;
+  font-size: 30px;
+  color: #000000;
+`;
+const ScopeIConWrap = styled.div``;
 // 리뷰 개수
 const Bottom = styled.p`
-    font-weight: 400;
-    font-size: 20px;
-    color: #999999;
-
-`
+  font-weight: 400;
+  font-size: 20px;
+  color: #999999;
+`;
