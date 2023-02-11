f8e36cbe01b5faf58b6beedffc1c9db97d0e1722
computerbomin <computer.bomin@gmail.com>
2023-02-11 22:13:15 +0900

    [CHANGE] 가게 상세 모달창
    [수정] 가게 상세 모달창 API 변경
    - 전체 음식점을 가져오는 거 -> 해당 id의 음식점 정보만 가져오는 거

--- front-end/src/api/RestaurantModalAPI.js
+++ front-end/src/api/RestaurantModalAPI.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const url = "http://3.35.140.28:9000/shop";
-
 export const RestaurantModalAPI = async (setListData, id) => {
+  const url = `http://3.35.140.28:9000/shop/${id}`;
+
   try {
     const data = await axios({
       method: "get",
       url: url,
     });
-    setListData(data.data.result[id - 1]);
+    setListData(data.data.result);
   } catch (err) {
     alert(err);
   }
