f0fed8b42b0340cdc64a95f0f6278c88e568c7f3
computerbomin <springin98@gmail.com>
2023-03-02 02:25:23 +0900

    [ADD] 디테일 모달창으로 넘어갔을 때, 오버레이 닫기

--- front-end/src/components/body/map/showMap/ShowMarker.jsx
+++ front-end/src/components/body/map/showMap/ShowMarker.jsx
@@ -107,6 +107,10 @@ const ShowMarker = (props) => {
         kakao.maps.event.addListener(_map, "click", function () {
           showOverlay.setMap(null);
         });
+        //오버레이 클릭했을 때 오버레이 닫기
+        contents.onclick = () => {
+          showOverlay.setMap(null);
+        };
       });
     });
   }, [props.mapData, openBookmark]);
